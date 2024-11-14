129636cc196ab3bb99059d1a2e12608dc2040c98
Kailash-Shankar <kailashshankar@ufl.edu>
2024-11-14 17:46:38 -0500

    Add files via upload

--- Painting_Game-Kailash_S.py
+++ Painting_Game-Kailash_S.py
@@ -12,10 +12,12 @@ screen = pygame.display.set_mode((600, 600))
 screen.fill("lightcoral")
 W = pygame.font.Font(None, 100)
 V = pygame.font.Font(None, 40)
-Name = V.render("By Kailash S. | 11/13/2024", 0, "black")
+Name = V.render("By Kailash S. | 11/14/2024", 0, "black")
 Gametext = W.render("Painting Game", 0, "midnightblue")
 Starttext = W.render("Start", 0, "white", "black")
 Starttext2 = W.render("Start", 0, "yellow", "darkslategrey")
+
+flag = 0
 
 GT_rect = Gametext.get_rect(center=(300, 200))
 ST_rect = Starttext.get_rect(center=((300, 400)))
@@ -53,6 +55,11 @@ def draw_screen():
     screen.fill(screen_color)
 draw_screen()
 SWATCHES = ["black", "red", "yellow", "blue", "seagreen", "darkorange", "orchid", "cyan", "gold", "maroon"]
+sizes = [5, 10, 15, 20]
+SIZE = sizes[1]
+depth = [470, 495, 530, 575]
+DEPTH = depth[1]
+
 val = []
 for i in range(len(SWATCHES)):
     val.append(25+i*50)
@@ -69,14 +76,20 @@ Y = pygame.font.Font(None, 25)
 Eraser = X.render("Erase", 0, "black", "silver")
 Clear_Screen = X.render("Clear", 0, "black", "white")
 BG_Change = Y.render("Change Screen Color", 0, "black", "white")
+Brush_Size = Y.render("Brush Size", 0, "black", "white")
 
 E_rect = Eraser.get_rect(center=(550, 50))
 C_rect = Clear_Screen.get_rect(center=(550, 100))
 B_rect = BG_Change.get_rect(center=(500, 550))
+BS_rect = Brush_Size.get_rect(center=(50, 450))
 def draw_block_commands():
     screen.blit(Eraser, E_rect)
     screen.blit(Clear_Screen, C_rect)
     screen.blit(BG_Change, B_rect)
+    screen.blit(Brush_Size, BS_rect)
+
+
+
 
 
 draw_block_commands()
@@ -85,18 +98,40 @@ draw_block_commands()
 
 def draw_highlight():
     pygame.draw.circle(screen, "white", (25, 50), 20, 5)
+
+def draw_highlight_sizes():
+    pygame.draw.circle(screen, "white", (40, DEPTH), SIZE, 4)
 draw_highlight()
+
 pygame.display.update()
 COLOR = SWATCHES[0]
+BrushSize = [[depth[0], sizes[0]], [depth[1], sizes[1]],[depth[2], sizes[2]], [depth[3], sizes[3]]]
+
+def draw_brush_sizes():
+    if COLOR != screen_color:
+        for i in range(len(BrushSize)):
+            pygame.draw.circle(screen, COLOR, (40, BrushSize[i][0]), BrushSize[i][1])
+    else:
+        for i in range(len(BrushSize)):
+            pygame.draw.circle(screen, "grey", (40, BrushSize[i][0]), BrushSize[i][1])
+
+draw_brush_sizes()
+draw_highlight_sizes()
+pygame.display.update()
 
 def setup():
     draw_screen()
+    global SIZE, DEPTH
+    SIZE, DEPTH = sizes[1], depth[1]
+    global COLOR
+    COLOR = SWATCHES[0]
     draw_block_commands()
     draw_swatches()
     draw_highlight()
+    draw_brush_sizes()
+    draw_highlight_sizes()
     pygame.display.update()
-    global COLOR
-    COLOR = "black"
+
 
 
 clock = pygame.time.Clock()
@@ -120,19 +155,33 @@ try:
                         if i - 20 <= x <= i + 20:
                             COLOR = SWATCHES[val.index(i)]
                             draw_swatches()
+                            draw_brush_sizes()
                             pygame.draw.circle(screen, "white", (i, 50), 20, 5)
+                            draw_highlight_sizes()
                             pygame.display.update()
                     #Checks for 'Erase' button press
                     if 500<x<600:
+                        COLOR = screen_color
                         draw_swatches()
+                        draw_brush_sizes()
+                        draw_highlight_sizes()
                         pygame.display.update()
                         COLOR = screen_color
+
                 #Checks for 'Change Screen Color' button press
                 elif 415<x<585 and 530<y<570:
                     SCL_index += 1
                     if SCL_index >= len(screen_color_list):
                         SCL_index = 0
+                    placeholder = COLOR if COLOR == screen_color else 0
+                    P2 = COLOR if placeholder == 0 else 7
                     screen_color = screen_color_list[SCL_index]
+                    if placeholder != 0 or flag == 1:
+                        COLOR = screen_color
+                        flag = 1
+                    else:
+                        flag = 0
+                        COLOR = P2 if P2 != 7 else screen_color
                     setup()
 
 
@@ -140,6 +189,15 @@ try:
                 elif 80<y<120 and 500<x<600:
                     setup()
 
+                elif 20<x<60:
+                    for i in range(len(depth)):
+                        if depth[i]-20<y<depth[i]+20:
+                            DEPTH = depth[i]
+                            SIZE = sizes[i]
+
+                            draw_brush_sizes()
+                            draw_highlight_sizes()
+                            pygame.display.update()
 
                 #Turns pen on
                 else:
@@ -149,7 +207,7 @@ try:
             #Uses pen
             if down:
                 x, y = pygame.mouse.get_pos()
-                pygame.draw.circle(screen, COLOR, (x, y), 10)
+                pygame.draw.circle(screen, COLOR, (x, y), SIZE)
                 pygame.display.update()
 
             #Turns pen off
